25f1d15c03029d33c70306f4cc95f51aaf9abd64
satvik <patilsatvik66@example.com>
2026-01-27 22:55:01 +0530

    Added UserDetailsService

--- Url-shortner_Fullstack/src/main/java/com/satvik/url_shortner/security/JwtAuthenticationFilter.java
+++ Url-shortner_Fullstack/src/main/java/com/satvik/url_shortner/security/JwtAuthenticationFilter.java
@@ -12,10 +12,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
@@ -43,7 +45,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }finally {
+            filterChain.doFilter(request, response);
         }
+
 
     }
 }
---
+++ Url-shortner_Fullstack/src/main/java/com/satvik/url_shortner/service/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.satvik.url_shortner.service;
+
+import com.satvik.url_shortner.models.User;
+import com.satvik.url_shortner.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+        return UserDetailsImpl.build(user);
+    }
+}
